3345d1b87a64a09e0ba1af6e94d26e7fa9005cdb
Ednilson G Rossi <ednilsonrossi@ifsp.edu.br>
2019-02-25 19:21:01 -0300

    Correcao Ex 2

--- Main.java
+++ Main.java
@@ -1,4 +1,15 @@
-
+/*
+ * Cristina, observe que como usou o eclipse ou netbeans para programar foi definido um 
+ * pacote (triangulopoo), por isso, seu sistema não funciona da forma que está.
+ *
+ * Evite utilizar interação com o usuário em suas classes de negócio, nesse exercício você 
+ * usou JOptionPane na classe TrianguloPoo, isso implica que se eu desejar utilizar sua classe
+ * em outro tipo de aplicação não será possível. Lembre-se sempre da portabilidade.
+ *
+ * De forma geral o programa resolve o problema.
+ * 
+ * Nota 10,0
+ */
 package triangulopoo;
 
 import javax.swing.JOptionPane;
